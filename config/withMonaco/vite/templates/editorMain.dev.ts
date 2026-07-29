62a1a5108e596f72c9439ecf6ced1a0ace696937
Alban G. <oxybo.prog@gmail.com>
2026-07-29 22:10:05 +0200

    feat: Implement worker caching in Monaco environment; optimize worker retrieval and loading

--- config/withMonaco/vite/templates/editorMain.dev.ts
+++ config/withMonaco/vite/templates/editorMain.dev.ts
@@ -11,6 +11,8 @@ export const generateMain = ({
     languagesImports: string[];
     globalAPI: boolean;
 }) => `
+const cached = new Map();
+
 self['MonacoEnvironment'] = {
     globalAPI: ${globalAPI || false},
     getWorker: ((workers) => (_, label) => {
@@ -18,10 +20,16 @@ self['MonacoEnvironment'] = {
             console.warn('[monaco] no worker found for label', label);
             return null;
         }
+
+        if (cached.has(label)) return cached.get(label);
         
         const url = browser.runtime.getURL(workers[label]);
         console.log('[monaco] loading worker', label, url);
-        return new Worker(url, { type: 'module' });
+
+        const worker = new Worker(url, { type: 'module' });
+        cached.set(label, worker);
+
+        return worker;
     })({
         ${workersObject.join(',\n        ')}
     })
